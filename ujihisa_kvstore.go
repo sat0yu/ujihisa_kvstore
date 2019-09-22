6f3f87641497b63103d47ff951e2c850ea27617a
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2019-09-22 21:39:32 +0900

    enable to launch the server with listening any port

--- ujihisa_kvstore.go
+++ ujihisa_kvstore.go
@@ -2,22 +2,27 @@ package main
 
 import (
     "bytes"
+    "flag"
     "fmt"
     "net/http"
+    "strconv"
+    "strings"
 )
 
 type KVStore map[string]string
 
 var datastore KVStore
+var ownPort int
+var mirrorPorts []int
 
 func getHandler(w http.ResponseWriter, r *http.Request) {
     key := r.URL.Path
     v, ok := datastore[key]
     if ok {
-        fmt.Printf("Hit: key=%s with value=%v\n", key, v)
+        fmt.Printf("Hit(:%d): key=%s with value=%v\n", ownPort, key, v)
         fmt.Fprintf(w, v)
     } else {
-        fmt.Printf("Missing: key=%s\n", key)
+        fmt.Printf("Missing(:%d): key=%s\n", ownPort, key)
         fmt.Fprintf(w, "not found")
     }
 }
@@ -29,9 +34,9 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
     v := buf.String()
     _, ok := datastore[key]
     if ok {
-        fmt.Printf("Updatnig: ")
+        fmt.Printf("Updatnig(:%d): ", ownPort)
     } else {
-        fmt.Printf("Inserting: ")
+        fmt.Printf("Inserting(:%d): ", ownPort)
     }
     fmt.Printf("key=%s with value=%v\n", key, v)
     datastore[key] = v
@@ -39,17 +44,43 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 
 func handler(w http.ResponseWriter, r *http.Request) {
     switch r.Method {
-    case "GET":
-        getHandler(w, r)
-    case "POST":
-        postHandler(w, r)
-    default:
-        fmt.Printf("found unsupported method")
+    case "GET": getHandler(w, r)
+    case "POST": postHandler(w, r)
+    default: fmt.Printf("found unsupported method")
     }
 }
 
 func main() {
+	var (
+	    port = flag.Int("p", 8000, "the port to which the process listens")
+	    mirrors = flag.String("m", "8000,8001,8002", "the accessible ports")
+    )
+    flag.Parse()
+	ownPort = *port
+	mirrorPorts = []int{}
+	for _, origPort := range strings.Split(*mirrors, ",") {
+        p, err := strconv.Atoi(origPort)
+        if err != nil {
+        	panic("given invalid mirror ports")
+        }
+	    mirrorPorts = append(mirrorPorts, p)
+    }
+	var included = false
+	for _, p := range mirrorPorts {
+	    if ownPort == p {
+	    	included = true
+		}
+    }
+	if !included {
+		panic("found invalid port")
+	}
+
+
+	fmt.Printf("Port: %d\n", ownPort)
+    fmt.Printf("Mirroring ports: %v\n", mirrorPorts)
+
     datastore = KVStore{}
     http.HandleFunc("/", handler)
-    http.ListenAndServe(":8001", nil)
+    addr := fmt.Sprintf(":%d", *port)
+    http.ListenAndServe(addr, nil)
 }
